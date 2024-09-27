a99eeef3f7a2c717d97468ff0ca691bb1230ad24
Thirdbot <astroll656@gmail.com>
2024-09-27 15:58:28 +0700

    ModItem DemoIcons

---
+++ src/main/java/com/third/demo/block/ModBlocks.java
@@ -0,0 +1,25 @@
+package com.third.demo.block;
+
+import com.third.demo.Demo;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.MapColor;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModBlocks {
+
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, Demo.MODID);
+
+    public static void register(IEventBus eventBus)
+    {
+        BLOCKS.register(eventBus);
+    }
+
+    // Creates a new Block with the id "demo:example_block", combining the namespace and path
+    public static final RegistryObject<Block> EXAMPLE_BLOCK = BLOCKS.register("example_block", () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.STONE)));
+
+
+}
--- src/main/java/com/third/demo/item/ModItems.java
+++ src/main/java/com/third/demo/item/ModItems.java
@@ -1,6 +1,8 @@
 package com.third.demo.item;
 
 import com.third.demo.Demo;
+import com.third.demo.block.ModBlocks;
+import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -14,8 +16,6 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, Demo.MODID);
 
-
-
     //Register Bus for items
     public static void register(IEventBus eventBus)
     {
@@ -25,6 +25,13 @@ public class ModItems {
     //Item Properties
 
     public static final RegistryObject<Item> DEMO_ICONS = ITEMS.register("demo_icons", () -> new Item(new Item.Properties()));
+
+    // Creates a new BlockItem with the id "demo:example_block", combining the namespace and path
+    public static final RegistryObject<Item> EXAMPLE_BLOCK_ITEM = ITEMS.register("example_block", () -> new BlockItem(ModBlocks.EXAMPLE_BLOCK.get(), new Item.Properties()));
+
+    // Creates a new food item with the id "examplemod:example_id", nutrition 1 and saturation 2
+    public static final RegistryObject<Item> EXAMPLE_ITEM = ITEMS.register("example_item", () -> new Item(new Item.Properties().food(new FoodProperties.Builder()
+            .alwaysEat().nutrition(1).saturationMod(2f).build())));
 
 
 }
